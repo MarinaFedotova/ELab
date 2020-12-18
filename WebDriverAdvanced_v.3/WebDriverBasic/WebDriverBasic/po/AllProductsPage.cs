d049f9c0ae2a93b9a877c83b4394b233ffd90149
Marina <m.fedotova1911@mail.ru>
2020-12-18 17:23:22 +0300

    WebDriverAdvanced_v.3

---
+++ WebDriverAdvanced_v.3/WebDriverBasic/WebDriverBasic/po/AllProductsPage.cs
@@ -0,0 +1,63 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebDriverBasic.po.page_components;
+
+namespace WebDriverBasic.po
+{
+    class AllProductsPage
+    {
+        private IWebDriver driver;
+        public NavBar navBar;
+
+        public AllProductsPage(IWebDriver driver)
+        {
+            this.driver = driver;
+            navBar = new NavBar(driver);
+        }
+
+        private int CountProducts => driver.FindElements(By.XPath("//table//tr")).Count;
+        private IWebElement CreateNewButton => driver.FindElement(By.LinkText("Create new"));
+
+        public int GetCountProducts()
+        {
+            return CountProducts;
+        }
+
+        public ProductPage ClickOnCreateNew()
+        {
+            new Actions(driver).SendKeys(CreateNewButton, Keys.Enter).Build().Perform();
+            return new ProductPage(driver);
+        }
+
+        public bool ProductInTable(int index)
+        {
+            try
+            {
+                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(3));
+                wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.InvisibilityOfElementLocated(By.XPath("//table//tr[" + index + "]")));
+                return false;
+            }
+            catch (Exception){return true;}
+        }
+
+        public ProductPage ClickOnLinkProduct(int index)
+        {
+            driver.FindElement(By.XPath("//table//tr[" + index + "]/td[2]/a")).Click();
+            return new ProductPage(driver);
+        }
+
+        public void ClickOnRemoveProduct(int index)
+        {
+            driver.FindElement(By.XPath("//table//tr[" + index + "]//a[text()=\"Remove\"]")).Click();
+        }
+
+        public void ClickOnYesPopUpWindow()
+        {
+            driver.SwitchTo().Alert().Accept();
+        }
+    }
+}
---
+++ WebDriverAdvanced_v.3/WebDriverBasic/WebDriverBasic/po/page_components/NavBar.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebDriverBasic.po.page_components
+{
+    class NavBar
+    {
+        private IWebDriver driver;
+
+        public NavBar(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        IWebElement ProductsLink => driver.FindElement(By.XPath("//li//a[text()=\"Products\"]"));
+        IWebElement LogoutLink => driver.FindElement(By.LinkText("Logout"));
+
+        public MainPage ClickOnLinkLogout()
+        {
+            new Actions(driver).Click(LogoutLink).Build().Perform();
+            return new MainPage(driver);
+        }
+
+        public AllProductsPage ClickOnLinkProducts()
+        {
+            new Actions(driver).Click(ProductsLink).Build().Perform();
+            return new AllProductsPage(driver);
+        }
+    }
+}
