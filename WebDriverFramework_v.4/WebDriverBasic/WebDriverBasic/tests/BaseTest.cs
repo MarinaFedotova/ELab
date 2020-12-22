e64e4ff7995c2411224a64a2f344fe3568038482
Marina <m.fedotova1911@mail.ru>
2020-12-22 19:17:05 +0300

    WebDriverFrameworkEnd_v.4

---
+++ WebDriverFramework_v.4/WebDriverBasic/WebDriverBasic/tests/BaseTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebDriverBasic.tests
+{
+    public class BaseTest
+    {
+        protected IWebDriver driver;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://localhost:5000");
+        }
+
+        [OneTimeTearDown]
+        public void CloseDriver()
+        {
+            driver.Close();
+            driver.Quit();
+        }
+    }
+}
